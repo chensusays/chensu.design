8d4a37239225838646a8a38da61f2a294c34b4ae
chensusays <chensusays@gmail.com>
2017-05-25 23:41:59 -0400

    jquery fix, scroll spy and css bugs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+ 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { DownloadComponent } from './download/download.component';
