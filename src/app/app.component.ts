304e4e0d1676446852aaaaa199353ea12cb2be1d
chensusays <chensusays@gmail.com>
2017-06-14 22:56:29 -0400

    added freeCodeCamp icon
    coded css for freeCodeCamp, luckily there was a font awesome icon
    created for freeCodeCamp!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,23 +14,28 @@ export class AppComponent {
 
   skills = "Java, Javascript, Ruby, HTML, CSS, C, C#, Angular 2, React JS, Asp.Net, MySQL, Scala, PHP, Python, Illustrator, Photoshop";
 
-  education = "University of Pittsburgh - Class of 2017";
+  freeCodeCamp = "freeCodeCamp | 2017 - 2018";
+  degree = "Full Stack Web Development Certification";
+  fieldOfStudy = "Computer Software Engineering";
+
+  education = "University of Pittsburgh | Class of 2017";
   major = "Bachelor's of Science in Computer Engineering";
 
-  cloudpcrrole = "Junior Software Engineer (Spring)";
-  cloudpcrduration = "CloudPCR | Pittsburgh, PA | Jan 2017 - April 2017";
-  cloudpcrdescription = "Built new and improved existing Angular 2 and ASP.NET form components in multiple different stacks.";
+
+  cloudpcrRole = "Junior Software Engineer (Spring)";
+  cloudpcrDuration = "CloudPCR | Pittsburgh, PA | Jan 2017 - April 2017";
+  cloudpcrDescription = "Built new and improved existing Angular 2 and ASP.NET form components in multiple different stacks.";
   
-  philipsrole = "Software Engineer Co-op";
-  philipsduration = "Philips Respironics | Pittsburgh, PA | Jan 2016 - August 2016";
-  philipsdescription = "Using JavaScript, C#, and Bootstrap on ASP.NET, I helped release the Japanese version of the web and mobile app.<br>Wrote an automation script using C# and SQL to scrape information from Microsoft TFS.<br>Learned unit testing with Jasmine.js.";
+  philipsRole = "Software Engineer Co-op";
+  philipsDuration = "Philips Respironics | Pittsburgh, PA | Jan 2016 - August 2016";
+  philipsDescription = "Using JavaScript, C#, and Bootstrap on ASP.NET, I helped release the Japanese version of the web and mobile app.<br>Wrote an automation script using C# and SQL to scrape information from Microsoft TFS.<br>Learned unit testing with Jasmine.js.";
 
-  bnyrole = "Software Engineer Intern";
-  bnyduration = "BNY Mellon | Pittsburgh, PA | May 2015 - Aug 2015";
-  bnydescription = "Front-end redesign of Internal Audit Division’s main website to be responsive.<br>Created automated emails to appropriate users based upon form results and MySQL queries.<br>Implemented a Visual Basic automation script to populate a organized Microsoft Excel workbook using unformatted data.";
+  bnyRole = "Software Engineer Intern";
+  bnyDuration = "BNY Mellon | Pittsburgh, PA | May 2015 - Aug 2015";
+  bnyDescription = "Front-end redesign of Internal Audit Division’s main website to be responsive.<br>Created automated emails to appropriate users based upon form results and MySQL queries.<br>Implemented a Visual Basic automation script to populate a organized Microsoft Excel workbook using unformatted data.";
 
-  pittrole = "Front-End Web Developer";
-  pittduration = "Pitt Ultimate | Pittsburgh, PA | May 2013 - Aug 2013";
-  pittdescription = "Learned HTML, CSS, JavaScript, and PHP to design website for the University of Pittsburgh's Men's Ultimate Frisbee program.";
+  pittRole = "Front-End Web Developer";
+  pittDuration = "Pitt Ultimate | Pittsburgh, PA | May 2013 - Aug 2013";
+  pittDescription = "Learned HTML, CSS, JavaScript, and PHP to design website for the University of Pittsburgh's Men's Ultimate Frisbee program.";
 
 }
